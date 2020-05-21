239799e7f8eaddc985a0cebe5e074ce73a6decc7
Thomas Boutell <tom@apostrophecms.com>
2020-05-21 14:42:23 -0400

    support for custom filters in the pieces export dialog box

--- public/js/export-modal.js
+++ public/js/export-modal.js
@@ -15,11 +15,19 @@ apos.define('apostrophe-pieces-export-modal', {
         var draftOrLive = self.$el.find('[name="draft-or-live"]').val();
         var published = self.$el.find('[name="published"]').val();
         var extension = self.$el.find('[name="extension"]').val();
-        self.api('export', {
+        var data = {
           draftOrLive: draftOrLive,
           published: published,
           extension: extension
-        }, function (result) {
+        };
+        var $selects = self.$el.find('.apos-pieces-export-filter select');
+        $selects.each(function() {
+          var $select = $(this);
+          if ($select.val().length) {
+            data[$select.attr('name')] = $select.val();
+          }
+        });
+        self.api('export', data, function (result) {
           if (result.status !== 'ok') {
             apos.notify(result.status, { type: 'error' });
             return;
